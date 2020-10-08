95da3018448c1c2c7e6fe2072783b2140099d82f
tian <istiancz@gmail.com>
2020-10-08 14:55:01 +0800

    add react&reactDom 😍

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,5 @@
-const div=document.createElement('div')
-div.innerText='disssss'
-document.body.appendChild(div)
+import React from 'react'
+import ReactDom from 'react-dom'
+
+console.log(React)
+console.log(ReactDom)
