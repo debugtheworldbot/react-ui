0c5aff060adef53d165e7f8914d85639d20ebc50
tian <istiancz@gmail.com>
2020-10-16 21:59:31 +0800

    update package.json

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,4 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Icon from './icon/icon'
 
+export {default as Icon} from './icon/icon'
 
-const fn:React.MouseEventHandler=(e)=>{
-    console.log(e.target)
-}
-ReactDom.render(
-    <div>
-        <Icon name={'alipay'}/>
-        <Icon name={'wechat'} className={'false'} onClick={fn}/>
-    </div>
-    , document.querySelector("#root"))
 
