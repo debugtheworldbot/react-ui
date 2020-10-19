9385d84160188104849e7b347625c28cc7adb432
tian <istiancz@gmail.com>
2020-10-19 21:45:55 +0800

    diy buttons

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,10 +3,13 @@ import Dialog from "./dialog";
 
 const DialogExample: React.FunctionComponent = () => {
     const [x, setX] = useState(false)
-
     return (
         <div>
-            <Dialog visible={x}>
+            <Dialog visible={x} buttons={[
+                <button>1</button>,
+                <button>2</button>,
+                <button>3</button>
+            ]} onClose={()=>setX(false)}>
                 <div>hi</div>
             </Dialog>
             <button onClick={()=>setX(!x)}>toggle</button>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, {ReactElement} from 'react'
 import './dialog.scss'
 import {Icon} from "../index";
 import {scopedClassMaker} from "../classes";
+
 interface Props {
     visible: boolean,
+    buttons?: Array<ReactElement>
+    onClose: React.MouseEventHandler
 }
 
 
 const scopedClass = scopedClassMaker('czUi-dialog')
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
+    const handleClose: React.MouseEventHandler = (e) => {
+        props.onClose(e)
+    }
+    const buttons=props.buttons&&props.buttons.length>0&& props.buttons.map((button, index) => {
+        return  React.cloneElement(button, {key: index})})
+
+
     return (
         props.visible ?
             <>
                 <div className={scopedClass('mask')}>a</div>
                 <div className={scopedClass()}>
-                    <div className={scopedClass('close')}>
-                        <Icon  name={'close'}/>
+                    <div className={scopedClass('close')} onClick={handleClose}>
+                        <Icon name={'close'}/>
                     </div>
                     <header className={scopedClass('header')}>hint</header>
                     <main className={scopedClass('main')}>{props.children}</main>
                     <footer className={scopedClass('footer')}>
-                        <button>ok</button>
-                        <button>cancel</button>
+                        {buttons? buttons:
+                            <div>
+                                <button>ok</button>
+                                <button>cancel</button>
+                            </div>}
+
                     </footer>
-                    </div>
+                </div>
             </>
             :
             null
