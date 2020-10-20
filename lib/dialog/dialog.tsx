67a4cad2ab032a78bed422bf880298688e5e9b7f
tian <istiancz@gmail.com>
2020-10-20 21:59:10 +0800

    react-dom portal

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -2,6 +2,7 @@ import React, {ReactElement} from 'react'
 import './dialog.scss'
 import {Icon} from "../index";
 import {scopedClassMaker} from "../classes";
+import ReactDOM from 'react-dom';
 
 interface Props {
     visible: boolean,
@@ -24,29 +25,30 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     }
     const buttons=props.buttons&&props.buttons.length>0&& props.buttons.map((button, index) => {
           return  React.cloneElement(button, {key: index})})
+    const dialog= props.visible ?
+        <>
+            <div className={scopedClass('mask')} onClick={handleMask} />
+            <div className={scopedClass()}>
+                <div className={scopedClass('close')} onClick={handleClose}>
+                    <Icon name={'close'}/>
+                </div>
+                <header className={scopedClass('header')}>hint</header>
+                <main className={scopedClass('main')}>{props.children}</main>
+                <footer className={scopedClass('footer')}>
+                    {buttons? buttons:
+                        <div>
+                            <button>ok</button>
+                            <button>cancel</button>
+                        </div>}
+
+                </footer>
+            </div>
+        </>
+        :
+        null
 
     return (
-        props.visible ?
-            <>
-                <div className={scopedClass('mask')} onClick={handleMask} />
-                <div className={scopedClass()}>
-                    <div className={scopedClass('close')} onClick={handleClose}>
-                        <Icon name={'close'}/>
-                    </div>
-                    <header className={scopedClass('header')}>hint</header>
-                    <main className={scopedClass('main')}>{props.children}</main>
-                    <footer className={scopedClass('footer')}>
-                        {buttons? buttons:
-                            <div>
-                                <button>ok</button>
-                                <button>cancel</button>
-                            </div>}
-
-                    </footer>
-                </div>
-            </>
-            :
-            null
+        ReactDOM.createPortal(dialog,document.body)
     )
 }
 export default Dialog
