48ce7fe72de347b95598140be21d29e40060d8d9
tian <istiancz@gmail.com>
2020-10-24 14:58:57 +0800

    get rid of LET

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,25 +1,20 @@
-import React, { ReactElement }  from 'react';
+import React, {ReactElement} from 'react';
 import {scopedClassMaker} from "../classes";
 import './layout.scss'
 import classes from "../helper/classes";
 import Aside from "./aside";
 
 const scopedClass = scopedClassMaker('czUi-layout')
-interface Props extends React.HTMLAttributes<HTMLElement>{
-    children:ReactElement|Array<ReactElement>
+
+interface Props extends React.HTMLAttributes<HTMLElement> {
+    children: ReactElement | Array<ReactElement>
 }
 
-const Layout: React.FunctionComponent<Props> = ({className,...restProps}) => {
-    let hasAside=false
-    if(restProps.children&&(restProps.children as Array<ReactElement>).length){
-        (restProps.children as Array<ReactElement>).map(child=>{
-            if(child.type === Aside){
-                hasAside=true
-            }
-        })
-
-    }
-    return <div className={classes(scopedClass(),className,hasAside ? scopedClass('hasAside'):undefined)}  {...restProps}>
+const Layout: React.FunctionComponent<Props> = ({className, ...restProps}) => {
+    const children = restProps.children && (restProps.children as Array<ReactElement>).length
+    const hasAside = children && (restProps.children as Array<ReactElement>).reduce((result, node) => result || node.type === Aside, false)
+    return <div
+        className={classes(scopedClass(), className, hasAside ? scopedClass('hasAside') : undefined)}  {...restProps}>
         {restProps.children}
     </div>
 }
