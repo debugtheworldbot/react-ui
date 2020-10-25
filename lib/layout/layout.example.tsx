6bb996bf72cebbc6abc0e563f2b196002f8683e3
tian <istiancz@gmail.com>
2020-10-25 21:42:49 +0800

    layout css

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -4,40 +4,41 @@ import Header from './header';
 import Content from "./content";
 import Aside from "./aside";
 import Footer from './footer';
+import './layout.example.scss'
 
 export default function () {
     return <>
         <h1>1</h1>
         <Layout>
-            <Header>header</Header>
-            <Content>content</Content>
-            <Footer>footer</Footer>
+            <Header className={'x'}>header</Header>
+            <Content className={'y'}>content</Content>
+            <Footer className={'x'}>footer</Footer>
         </Layout>
         <h1>2</h1>
         <Layout>
-            <Header>header</Header>
+            <Header className={'x'}>header</Header>
             <Layout>
-                <Aside>aside</Aside>
-                <Content>content</Content>
+                <Aside className={'a'}>aside</Aside>
+                <Content className={'z'}>content</Content>
             </Layout>
-            <Footer>footer</Footer>
+            <Footer className={'x'}>footer</Footer>
         </Layout>
         <h1>3</h1>
         <Layout>
-            <Header>header</Header>
+            <Header className={'x'}>header</Header>
             <Layout>
-                <Content>content</Content>
-                <Aside>aside</Aside>
+                <Content className={'z'}>content</Content>
+                <Aside className={'a'}>aside</Aside>
             </Layout>
-            <Footer>footer</Footer>
+            <Footer className={'x'}>footer</Footer>
         </Layout>
         <h1>4</h1>
         <Layout>
-            <Aside>aside</Aside>
+            <Aside className={'a'}>aside</Aside>
             <Layout>
-                <Header>header</Header>
-                <Content>content</Content>
-                <Footer>footer</Footer>
+                <Header className={'x'}>header</Header>
+                <Content className={'z'}>content</Content>
+                <Footer className={'x'}>footer</Footer>
             </Layout>
         </Layout>
 
