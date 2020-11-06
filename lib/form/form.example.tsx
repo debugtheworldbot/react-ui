0357155edfc95a7a2eee49ab240e60a43cb62efd
tian <istiancz@gmail.com>
2020-11-06 22:51:15 +0800

    finish submit

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,17 +1,17 @@
 import React, {useState} from 'react'
-import Form from "./form";
+import Form, {FormValue} from "./form";
 
 const FormExample: React.FunctionComponent = () => {
-    const [formData] = useState({username: '', password: ''})
+    const [formData,setFormData] = useState<FormValue>({username: '', password: ''})
     const [fields] = useState([
         {name: 'username', label: '姓名', input: {type: 'text'}},
         {name: 'password', label: '密码', input: {type: 'password'}},
     ])
     const handleSubmit= (e:React.FormEvent<HTMLFormElement>) =>{
-        console.log('submit')
+        console.log(e)
     }
     return (
-        <Form value={formData} fields={fields} onSubmit={handleSubmit} buttons={
+        <Form value={formData} fields={fields} onSubmit={handleSubmit} onChange={(newForm)=>setFormData(newForm)} buttons={
             <>
                 <button type='submit'>submit</button>
                 <button>cancel</button>
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,24 +1,32 @@
 import React, {ReactFragment} from 'react'
 
+export interface FormValue {
+    [key: string]: any
+}
 interface FormProps {
-    value: { [key: string]: any },
+    value: FormValue
     fields: Array<{ name: string, label: string, input: { type: string } }>,
     buttons: ReactFragment,
-    onSubmit: React.FormEventHandler<HTMLFormElement>
+    onSubmit: React.FormEventHandler<HTMLFormElement>,
+    onChange:(value:FormValue)=>void
 }
 
 const Form: React.FunctionComponent<FormProps> = (props) => {
-    const {fields, buttons, onSubmit} = props
+    const {fields, buttons, onSubmit,value,onChange} = props
     const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault()
         onSubmit(e)
+    }
+    const handleChange = (name:string,v:string) =>{
+        const newForm = {...value,[name]:v}
+        onChange(newForm)
     }
     return (
         <form onSubmit={handleSubmit}>
             {fields.map(field =>
                 <div key={field.name}>
                     {field.label}:
-                    <input type={field.input.type}/>
+                    <input type={field.input.type} value={value[field.name]} onChange={(e)=>handleChange(field.name,e.target.value)}/>
                 </div>)}
             <div>
                 {buttons}
