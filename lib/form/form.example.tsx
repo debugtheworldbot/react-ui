0a6c1b2e41a606b5be7b259698601c8ce2f6e45d
tian <istiancz@gmail.com>
2020-11-07 17:00:38 +0800

    validate is empty and short

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -3,26 +3,27 @@ import Form, {FormValue} from "./form";
 import Validator from "./validator";
 
 const FormExample: React.FunctionComponent = () => {
-    const [formData,setFormData] = useState<FormValue>({username: '', password: ''})
+    const [formData, setFormData] = useState<FormValue>({username: '', password: ''})
     const [fields] = useState([
         {name: 'username', label: '姓名', input: {type: 'text'}},
         {name: 'password', label: '密码', input: {type: 'password'}},
     ])
-    const handleSubmit= (e:React.FormEvent<HTMLFormElement>) =>{
-        const rules=[{
-            key:'username',
-            required:true
-        }]
-        const errors = Validator(formData,rules)
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        const rules = [
+            {key: 'username', required: true},
+            {key: 'username', minLength:8, maxLength:18},
+        ]
+        const errors = Validator(formData, rules)
         console.log(errors)
     }
     return (
-        <Form value={formData} fields={fields} onSubmit={handleSubmit} onChange={(newForm)=>setFormData(newForm)} buttons={
-            <>
-                <button type={'submit'}>submit</button>
-                <button type={'button'}>cancel</button>
-            </>
-        }/>
+        <Form value={formData} fields={fields} onSubmit={handleSubmit} onChange={(newForm) => setFormData(newForm)}
+              buttons={
+                  <>
+                      <button type={'submit'}>submit</button>
+                      <button type={'button'}>cancel</button>
+                  </>
+              }/>
     )
 }
 export default FormExample
--- lib/form/validator.tsx
+++ lib/form/validator.tsx
@@ -1,24 +1,37 @@
 import {FormValue} from "./form";
 
 interface FormRules {
-    key:string,
-    required?:boolean,
-    minLength?:number,
-    maxLength?:number
+    key: string,
+    required?: boolean,
+    minLength?: number,
+    maxLength?: number
 }
 
 interface FormErrors {
-    [key:string]:string[]
+    [key: string]: string[]
 }
 
-const Validator = (formValue:FormValue,rules:FormRules[]) :FormErrors=>{
-    let error:any= {}
-    rules.map(rule=>{
-        if(rule.required){
-           return  formValue[rule.key]?{}:error[rule.key]=['required!']
+const isEmpty = (value: any) => {
+    return value === undefined || value === '' || value === null
+}
+
+const Validator = (formValue: FormValue, rules: FormRules[]): FormErrors => {
+    let errors: any = {}
+    const addError = (message: string, key: string) => {
+        if (errors[key]===undefined) {
+            errors[key] = []
         }
-        return
+        errors[key].push(message)
+    }
+    rules.map(rule => {
+        const value = formValue[rule.key]
+        if (rule.required && isEmpty(value)) {
+            addError('required!', rule.key)
+        }
+        if (rule.minLength && !isEmpty(value) && value.length < rule.minLength) {
+            addError('short', rule.key)
+        }
     })
-    return  error
+    return errors
 }
 export default Validator
