38ac246337dd446fc35f3c7aca5ce47dd199c6f5
tian <istiancz@gmail.com>
2020-12-01 21:51:53 +0800

    create scroll bar

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { UIEventHandler } from 'react'
 import './scroll.scss'
 import scrollbarWidth from "./scrollbarWidth";
 
@@ -8,12 +8,18 @@ interface ScrollProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const Scroll: React.FunctionComponent<ScrollProps> = (props) => {
     const {children, ...rest} = props
+    const onScroll:UIEventHandler=(e)=>{
+        console.log(e.currentTarget.scrollTop)
+    }
     return (
         <div {...rest} className={'czUi-scroll'}>
-            <div className={'czUi-scroll-inner'} style={{right:-scrollbarWidth()}} >
+            <div className={'czUi-scroll-inner'} style={{right: -scrollbarWidth()}} onScroll={onScroll}>
                 {children}
             </div>
+            <div className={'czUi-scroll-track'}>
+                <div className="czUi-scroll-bar" />
+            </div>
         </div>
-    )
+    );
 }
 export default Scroll
