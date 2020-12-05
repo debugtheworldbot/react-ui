d6842a014d07700617d3930248dd08c7f0f0b721
tian <istiancz@gmail.com>
2020-12-05 12:43:53 +0800

    reformat code

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React, { UIEventHandler, useState, useEffect, useRef } from 'react'
+import React, {UIEventHandler, useState, useEffect, useRef} from 'react'
 import './scroll.scss'
 import scrollbarWidth from "./scrollbarWidth";
 
@@ -8,29 +8,28 @@ interface ScrollProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const Scroll: React.FunctionComponent<ScrollProps> = (props) => {
     const {children, ...rest} = props
-    const [h,setH]=useState(0)
-    const [t,setT]=useState(0)
+    const [barHeight, setBarHeight] = useState(0)
+    const [topDistance, setTopDistance] = useState(0)
     const containerRef = useRef<HTMLDivElement>(null)
 
-    const onScroll:UIEventHandler=(e)=>{
-        const viewHeight=containerRef.current!.getBoundingClientRect().height
-        const sHeight=e.currentTarget.scrollTop
-        const a = sHeight * h / viewHeight
-        setT(a)
+    const onScroll: UIEventHandler = (e) => {
+        const viewHeight = containerRef.current!.getBoundingClientRect().height
+        const sHeight = e.currentTarget.scrollTop
+        setTopDistance(sHeight * barHeight / viewHeight)
     }
-    useEffect(()=>{
-        const scrollHeight=containerRef.current!.scrollHeight
-        const viewHeight=containerRef.current!.getBoundingClientRect().height
-        const barHeight=viewHeight*viewHeight/scrollHeight
-        setH(barHeight)
-    },[])
+    useEffect(() => {
+        const scrollHeight = containerRef.current!.scrollHeight
+        const viewHeight = containerRef.current!.getBoundingClientRect().height
+        setBarHeight(viewHeight * viewHeight / scrollHeight)
+    }, [])
     return (
         <div {...rest} className={'czUi-scroll'}>
-            <div className={'czUi-scroll-inner'} ref={containerRef} style={{right: -scrollbarWidth()}} onScroll={onScroll}>
+            <div className={'czUi-scroll-inner'} ref={containerRef} style={{right: -scrollbarWidth()}}
+                 onScroll={onScroll}>
                 {children}
             </div>
             <div className={'czUi-scroll-track'}>
-                <div className="czUi-scroll-bar" style={{height:h,top:t}}/>
+                <div className="czUi-scroll-bar" style={{height: barHeight, top: topDistance}}/>
             </div>
         </div>
     );
